b44f712d5dfe21ae44c40ac4a2bd8206fe2ba7d0
insanhaka <insanhadikarunia@gmail.com>
2019-12-07 20:29:40 +0700

    arduino wifi

---
+++ wifinya_to_database.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid     = "INCENERATOR";
+const char* password = "123456789";
+const char* host = "192.168.1.10";
+const char* ip = "192.168.1.11";
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+}
+void loop() {
+  for (int level=1; level <= 5; level=level+1){
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  String url = "/serverTA/insert.php?level=" + String(level) + "&ip="+ String(ip);
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(500);
+  
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+  delay(3000);
+}
